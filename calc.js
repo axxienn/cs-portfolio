b40fc630714099a3ab3027459dd71b4c48f83d4a
Annie Deng <adeng.sh@mysummitps.org>
2016-11-04 21:10:24 +0000

    new 11/4

--- calc.js
+++ calc.js
@@ -1,4 +1,4 @@
-//calculator
+//calculator (gathering elements from HTML)
 var input = document.getElementById("input");
 var select = document.getElementById("operator");
 var input2 = document.getElementById("input2");
@@ -6,53 +6,57 @@ var button = document.getElementById("calculate");
 var ans = document.getElementById("answer");
 var answer;
 
-//calculator start
+//function of the calculator start
 button.addEventListener("click",function (){
+    var number = input.value;
+    var number2 = input2.value;
+    var operator = select.value;
     
-    if (select.value === "+"){
-    answer = parseInt(input.value) + parseInt(input2.value);
-    ans.innerHTML = answer;
+    if (operator === "+") {
+        var answer =  parseInt(number) + parseInt(number2);
+        ans.innerHTML = answer;
     }
     
-    if (select.value === "-"){
-    answer = parseInt(input.value) - parseInt(input2.value);
-    ans.innerHTML = answer;
+    if (operator === "-") {
+        var answer = parseInt(number) - parseInt(number2);
+        ans.innerHTML = answer;
     }
     
-    if (select.value === "*"){
-    answer = parseInt(input.value) * parseInt(input2.value);
-    ans.innerHTML = answer;
-    }
-   
-    if (select.value === "/"){
-    answer = parseInt(input.value) / parseInt(input2.value);
-    ans.innerHTML = answer;
-    } 
-    
-    if (select.value === "^"){
-    answer = Math.pow(input.value, input2.value);
-    ans.innerHTML = answer;
+    if (operator === "*") {
+        var answer = parseInt(number) * parseInt(number2);
+        ans.innerHTML = answer;
     }
     
-    if (select.value === "sqrt"){
-    answer = Math.sqrt(input2.value);
-    ans.innerHTML = answer;
+    if (operator === "/") {
+        var answer = parseInt(number) / parseInt(number2);
+        ans.innerHTML = answer;
     }
     
-    if (select.value === "sin"){
-    answer = Math.sin(input2.value);
-    ans.innerHTML = answer;
+    if (operator === "^") {
+        var answer = Math.pow(number, number2);
+        ans.innerHTML = answer;
     }
     
-    if (select.value === "cos"){
-    answer = Math.cos(input2.value);
-    ans.innerHTML = answer;
+    if (operator === "sqrt") {
+        var answer = Math.sqrt(number2);
+        ans.innerHTML = answer;
     }
     
-    if (select.value === "tan"){
-    answer = Math.tan(input2.value);
-    ans.innerHTML = answer;
+    if (operator === "sin") {
+        var answer = Math.sin(number2);
+        ans.innerHTML = answer;
     }
+    
+    if (operator === "cos") {
+        var answer = Math.cos(number2);
+        ans.innerHTML = answer;
+    }
+    
+    if (operator === "tan") {
+        var answer = Math.tan(number2);
+        ans.innerHTML = answer;
+    }
+    
 });
 
 
