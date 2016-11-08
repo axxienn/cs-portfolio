1c0d5c5a071d97839978b831012ff1549e23cc08
Annie Deng <adeng.sh@mysummitps.org>
2016-11-08 01:50:04 +0000

    new11/7

--- todo.js
+++ todo.js
@@ -1,1 +1,24 @@
-// JavaScript File
+//todolist(elements from HTML)
+var textbox = document.getElementById("textbox");
+var customize = document.getElementById("customize");
+var addbutton = document.getElementById("addbutton");
+var tasks = document.getElementById("tasks");
+
+//adding to do items
+addbutton.addEventListener("click", function() {
+    
+    var task = "<div>" + textbox.value + "<div>" + "<br>";
+    tasks.innerHTML = tasks.innerHTML + task; 
+    
+});
+
+//removing to do items
+tasks.addEventListener("click", function(evt){
+   
+   var remove = evt.target;
+   remove.parentNode.removeChild(remove);
+  
+});
+
+
+
